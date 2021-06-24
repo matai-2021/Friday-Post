83bbf1ec6cee2b3ac54e186392b687a83e7ca8d7
Westley Harrison <81056217+westley-ms-harrison@users.noreply.github.com>
2021-06-25 11:41:03 +1200

    add style for board

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -4,13 +4,22 @@ function Board () {
   return (
     <>
       <h1>My Board</h1>
-      <article>
-        <img src='https://solidstarts.com/wp-content/uploads/Lime_edited-480x320.jpg' />
-        <h3>Name</h3>
-        <p>Message</p>
-        <blockquote>Quote</blockquote>
-        <p>Time Stamp</p>
-      </article>
+      <section className='cards'>
+        <article className='card'>
+          <img src='https://solidstarts.com/wp-content/uploads/Lime_edited-480x320.jpg' />
+          <h3>Name</h3>
+          <p>Message</p>
+          <blockquote>Quote</blockquote>
+          <p>Time Stamp</p>
+        </article>
+        <article className='card'>
+          <img src='https://solidstarts.com/wp-content/uploads/Lime_edited-480x320.jpg' />
+          <h3>Name</h3>
+          <p>Message</p>
+          <blockquote>Quote</blockquote>
+          <p>Time Stamp</p>
+        </article>
+      </section>
     </>
   )
 }
